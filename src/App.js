72e73a13f0807035dca53c17c3cb9f2070e4e134
vijay13sagar <vijay13vidyasagar@gmail.com>
2022-03-09 23:39:50 -0500

    card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id='happy'>
+      <div className='frst'>
+     <p className='happy'>To the most loved Co-op Student co-ordinator🌺,
+     <br/><br/>
+     Thank you for being with me during all the highs and lows😍.
+     You changed my life and mindset through this period, which played a major part in my desperate search for jobs 💼, and I hope you will bring the best in other people through your unfaded smile 😊and positivity👍.
+<br/><br/>
+Happy women's day, best LIGHTBOARD user😅
+     </p>
+     <img className='dp' src='https://media-exp1.licdn.com/dms/image/C4E03AQHwlzg210rm8Q/profile-displayphoto-shrink_800_800/0/1576982359380?e=1652313600&v=beta&t=PlMIpmFVI1Q4UIngcDIotHsK7NSAoZJev9ysINBcrt0'/>
+     </div>
+     <div className='card' ><img className='cardimg' src='https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,w_730/at%2Farchive%2F31c08ba74d72b17633546cc81d06714402b090e8' /></div>
+     <div className='card im1' ><img className='cardimg' src='https://assets.eflorist.com/site/EF-22461/Tile/m7360340-pexels-photo-305087.jpeg' /></div>
+     <div className='card im2' ><img className='cardimg' src='https://cdn.pixabay.com/photo/2013/07/21/13/00/rose-165819__340.jpg'/></div>
+     <div className='card im3' ><img className='cardimg' src='https://images.unsplash.com/photo-1604085572504-a392ddf0d86a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8b3JhbmdlJTIwZmxvd2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80'/></div>
+     <div className='card im4' ><img className='cardimg' src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-red-rose-royalty-free-image-672727875-1546363000.jpg?crop=0.384xw:0.719xh;0.306xw,0.242xh&resize=480:*'/></div>
+     <div className='card im5' ><img className='cardimg' src='https://images.pexels.com/photos/850359/pexels-photo-850359.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'/></div>
+     <div className='card im6' ><img className='cardimg' src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/getty-aster-flower-meanings-1639330776.jpg?crop=0.4444444444444444xw:1xh;center,top&resize=480:*'/></div>
+     <div className='card im7' ><img className='cardimg' src='https://cdn.wallpapersafari.com/49/33/IkH9LK.jpg'/></div>
+     <div className='card im8' ><img className='cardimg' src='https://gardenerspath.com/wp-content/uploads/2019/01/Green-Flowers-From-Beautiful-to-Bizarre.jpg'/></div>
     </div>
   );
 }
